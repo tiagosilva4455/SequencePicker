166d26782c43a282d2cdf48388cceea9796abd2d
tiagosilva4455 <104692078+tiagosilva4455@users.noreply.github.com>
2022-05-09 19:14:33 +0100

    Add files via upload

---
+++ Database_Sequence_Picker_Pydna.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 21:56:17 2022
+
+@author: Tiago Silva
+"""
+
+import Bio
+import Bio.Blast
+
+from Bio.Blast import NCIBWWW
+
+my_seq = input("What is the sequence?")
