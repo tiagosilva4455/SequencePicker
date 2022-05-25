30e648f548cd971765ba0a5165515f05e7546251
tiagosilva4455 <104692078+tiagosilva4455@users.noreply.github.com>
2022-05-26 00:38:26 +0100

    Update Database_Sequence_Picker_Pydna.py

--- Database_Sequence_Picker_Pydna.py
+++ Database_Sequence_Picker_Pydna.py
@@ -5,33 +5,35 @@ Created on Tue May  3 21:56:17 2022
 @author: Tiago Silva
 """
 
-from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
-
-seq = "ctgacgctca"
-
-# def genbankref(seq):
+def genbankref(seq):
     
-#Blast
-
-result_handle = NCBIWWW.qblast("blastn", "nt", seq)
-
-blast_records = NCBIXML.parse(result_handle)
-
-print(result_handle)
+     from Bio.Blast import NCBIWWW
+     from Bio.Blast import NCBIXML 
+     from Bio import Entrez
+     from Bio import SeqIO
+  
+     result_handle = NCBIWWW.qblast("blastn", "nt", seq) #BLAST
     
-#Saving results
+     with open("blast_result.xml", "w+") as save_to:
+        save_to.write(result_handle.read())
+        result_handle.close() #SAVING RESULTS
+        
+        accessions=[]
+        with open("blast_result.xml", "r") as result_handle:
+            blast_record = NCBIXML.read(result_handle)
+            for h in blast_record.alignments:
+                accessions.append(h.accession) #OPENING RESULTS AND ACCESSION NUMBERS
+        
+        Entrez.email = "tjls2001@gmail.com"
 
-#with open("blast_results.xml", "w+") as save_to:
-    #save_to.write(result_handle.read())
-    #result_handle.close()
-    
-#Opening results
-
-#result_handle=open("blast_results.xml","r")
-#blast_record=NCBIXML.read(result_handle)
-#print(blast_record)
-
-
-
+        with Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=accessions) as handle:
+            for record in SeqIO.parse(handle,"gb"):
+                print("Definition: " + record.description)
+                print("Accession Number: " + record.id)
+                print("Start Site: " + record.seq[:15] + "...")
+                print("Stop Site:" + "..." + record.seq[-15:])
+                print()
+                if accessions == "":
+                    break #PRINTING START AND STOP SITES
+        
 
