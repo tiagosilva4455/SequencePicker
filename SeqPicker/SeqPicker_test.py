faab549c6a3c3be7fd4a38d32f1d7ba3a2e27393
tiagosilva4455 <104692078+tiagosilva4455@users.noreply.github.com>
2022-06-14 22:23:57 +0100

    Update SeqPicker_test.py

--- SeqPicker/SeqPicker_test.py
+++ SeqPicker/SeqPicker_test.py
@@ -1,1 +1,20 @@
+"""
+Created on Fri Jun  3 16:33:49 2022
+
+@author: Tiago Silva
+"""
+
+import pytest
+from SeqPicker import genbankref
+
+def test_noseq():
+    assert genbankref("") == "BLAST run failed." #Working on it
+
+def test_invalidNucleotideSequence():
+    assert genbankref("agjdhgou8ieyoquh!?") == "Please enter a valid nucleotide sequence"
+    
+def test_seqSameNucleotide():
+    assert genbankref("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") == "BLAST run failed." #WORKING ON IT
+    
+retcode = pytest.main()
 
