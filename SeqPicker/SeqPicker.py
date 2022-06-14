4d53265d485016c60c066db3d34cd19e62d614ef
tiagosilva4455 <104692078+tiagosilva4455@users.noreply.github.com>
2022-06-14 22:28:29 +0100

    Update SeqPicker.py

--- SeqPicker/SeqPicker.py
+++ SeqPicker/SeqPicker.py
@@ -15,41 +15,50 @@ def genbankref(seq):
         return True
     
     if validSequence(seq) == False:
-        return "Please enter a valid nucleotide sequence"  
+        return "Please enter a valid nucleotide sequence"
+    
     else:
-     
+    
      from Bio.Blast import NCBIWWW
      from Bio.Blast import NCBIXML 
      from Bio import Entrez
      from Bio import SeqIO
-  
-     try:
-         result_handle = NCBIWWW.qblast("blastn", "nt", seq) #BLAST
-     except Exception:
-             return "BLAST run failed."
+     
+     result_handle = NCBIWWW.qblast("blastn", "nt", seq) #BLAST
+   
          
-             with open("blast_result.xml", "w+") as save_to:
-                save_to.write(result_handle.read())
-                result_handle.close() #SAVING RESULTS
+    with open("blast_result.xml", "w+") as save_to:
+        save_to.write(result_handle.read())
+        result_handle.close() #SAVING RESULTS
                 
-                E_VALUE_THRESH = 0.00000000001
-                accessions=[]
+    E_VALUE_THRESH = 0.00000000001
+            
+    accessions=[]
+                
+    e_value=[]
                
-                with open("blast_result.xml", "r") as result_handle:
-                    blast_record = NCBIXML.read(result_handle)
-                    for alignment in blast_record.alignments:
-                            for hsp in alignment.hsps:
-                                if hsp.expect < E_VALUE_THRESH:
-                                    accessions.append(alignment.accession)#OPENING RESULTS AND ACCESSION NUMBERS
+    with open("blast_result.xml", "r") as result_handle:
+        blast_record = NCBIXML.read(result_handle)
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect < E_VALUE_THRESH:
+                    accessions.append(alignment.accession)
+                    e_value.append(hsp.expect) #OPENING RESULTS AND ACCESSION NUMBERS
+                                    
+    
            
-                Entrez.email = "tjls2001@gmail.com"
+    Entrez.email = "tjls2001@gmail.com"
         
-                with Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=accessions[0]) as handle:
-                    for record in SeqIO.parse(handle,"gb"):
-                        print("Definition: " + record.description)
-                        print("Accession Number: " + record.id)
-                        print("Start Site: " + record.seq[:15] + "...")
-                        print("Stop Site: " + "..." + record.seq[-15:])
-                        print()
-                        if accessions == "":
-                            break #PRINTING START AND STOP SITES
+    with Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=accessions[0]) as handle:
+        for record in SeqIO.parse(handle,"gb"):
+            print("Definition: " + record.description)
+            print("Accession Number: " + record.id)
+            print("Start Site: " + record.seq[:15] + "...")
+            print("Stop Site: " + "..." + record.seq[-15:])
+            print()
+            print(accessions)
+            print()
+            print(e_value)
+            if accessions == "":
+                break #PRINTING START AND STOP SITES
+                    
